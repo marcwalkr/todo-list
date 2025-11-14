56c86303fcbd9060b950e9b9b9da792d3b703ad9
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-11-14 11:39:04 -0600

    Rename themeToggle to themeToggleBtn for clarity

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./styles.css";
 
 const html = document.querySelector("html");
-const themeToggle = document.querySelector(".sidebar__theme-toggle-btn");
+const themeToggleBtn = document.querySelector(".sidebar__theme-toggle-btn");
 
-themeToggle.addEventListener("click", () => {
+themeToggleBtn.addEventListener("click", () => {
   const previousTheme = html.dataset.theme;
   const newTheme = previousTheme === "dark" ? "light" : "dark";
 
   html.dataset.theme = newTheme;
-  themeToggle.setAttribute("aria-label", `Switch to ${previousTheme} mode`);
+  themeToggleBtn.setAttribute("aria-label", `Switch to ${previousTheme} mode`);
 });
