2d9d2e0ab75e585c7404b018caaa4f34b97572dc
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-12-01 14:37:24 -0600

    Add ProjectStore module exposing create function

---
+++ src/projectStore.js
@@ -0,0 +1,23 @@
+import { createProject } from "./project.js";
+import { createUniqueId } from "./utils";
+
+const ProjectStore = (() => {
+  const projects = [];
+
+  const getIds = () => new Set(projects.map(p => p.id));
+
+  const add = (project) => {
+    projects.push(project);
+  };
+
+  const create = (name, color) => {
+    const id = createUniqueId(8, getIds());
+    const project = createProject(id, name, color);
+    add(project);
+    return project;
+  };
+
+  return { create };
+})();
+
+export default ProjectStore;
